431e6a48cffe97adfbd9f3896b196189637d743a
Keshavkant <keshavkantsharma111@gmail.com>
2021-06-06 16:25:30 +0100

    Sequence Images test.py

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+
+camera.start_preview()
+for i in range(5):
+	sleep(2)
+	camera.capture('/home/pi/Desktop/image%03d.jpg' % i) #Defines the name of the file.
+camera.stop_preview()
+
 
